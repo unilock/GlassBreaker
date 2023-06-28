79e0b25bad4a1f5e359b5bdbc17c25e5bb93ad91
unilock <unilock@fennet.rentals>
2023-06-28 13:39:36 -0400

    Update for 1.20
    Signed-off-by: unilock <unilock@fennet.rentals>

--- common/src/main/java/cc/unilock/glassbreaker/mixin/MixinMiningToolItem.java
+++ common/src/main/java/cc/unilock/glassbreaker/mixin/MixinMiningToolItem.java
@@ -1,9 +1,9 @@
 package cc.unilock.glassbreaker.mixin;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Material;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.MiningToolItem;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.item.*;
+import net.minecraft.sound.BlockSoundGroup;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,17 +17,59 @@ public class MixinMiningToolItem {
     @Final
     protected float miningSpeed;
 
-    @Inject(at = @At("HEAD"), method = "getMiningSpeedMultiplier(Lnet/minecraft/item/ItemStack;Lnet/minecraft/block/BlockState;)F", cancellable = true)
-    private void miningSpeedGlass(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-        if (state.getMaterial().equals(Material.GLASS)) {
+    @Inject(method = "getMiningSpeedMultiplier", at = @At("HEAD"), cancellable = true)
+    private void glassbreaker$getMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
+        if (glassbreaker$isGlass(state)) {
             cir.setReturnValue(this.miningSpeed);
         }
     }
 
-    @Inject(at = @At("HEAD"), method = "isSuitableFor(Lnet/minecraft/block/BlockState;)Z", cancellable = true)
-    private void suitableForGlass(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-        if (state.getMaterial().equals(Material.GLASS)) {
+    @Inject(method = "isSuitableFor", at = @At("HEAD"), cancellable = true)
+    private void glassbreaker$isSuitableFor(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        if (glassbreaker$isGlass(state)) {
             cir.setReturnValue(true);
         }
     }
+
+    private static boolean glassbreaker$isGlass(BlockState state) {
+        return state.getSoundGroup().equals(BlockSoundGroup.GLASS)
+                || state.getInstrument().equals(Instrument.HAT);
+    }
+
+    /*
+     * TODO: optimize this?
+     *  currently, it's likely very wasteful when mining any block that ISN'T glass
+     *  (though i'm not sure how one would fix that)
+     *
+    private static boolean glassbreaker$isGlassByTag(BlockState state) {
+        if (
+            // - fallbacks (loader-agnostic)
+                state.getSoundGroup().equals(BlockSoundGroup.GLASS)
+                || state.getInstrument().equals(Instrument.HAT)
+                // - block tags
+                // common tags
+                || state.isIn(TagKey.of(RegistryKeys.BLOCK, Identifier.of("c", "glass")))
+                || state.isIn(TagKey.of(RegistryKeys.BLOCK, Identifier.of("c", "glass_blocks")))
+                // forge tags
+                || state.isIn(TagKey.of(RegistryKeys.BLOCK, Identifier.of("forge", "glass")))
+                || state.isIn(TagKey.of(RegistryKeys.BLOCK, Identifier.of("forge", "glass_panes")))
+        ) {
+            return true;
+        }
+
+        // the below conditional is separate so we can store the default stack
+        // instead of having to get() it each time
+        ItemStack stateStack = state.getBlock().asItem().getDefaultStack();
+
+        return  // - item tags
+                // common tags
+                stateStack.isIn(TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "glass")))
+                || stateStack.isIn(TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "glass_blocks")))
+                || stateStack.isIn(TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "glass_pane")))
+                || stateStack.isIn(TagKey.of(RegistryKeys.ITEM, Identifier.of("c", "glass_panes")))
+                // forge tags
+                || stateStack.isIn(TagKey.of(RegistryKeys.ITEM, Identifier.of("forge", "glass")))
+                || stateStack.isIn(TagKey.of(RegistryKeys.ITEM, Identifier.of("forge", "glass_panes")));
+    }
+     */
 }
