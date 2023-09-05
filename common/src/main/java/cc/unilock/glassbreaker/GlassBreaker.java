2412ab892c75d1745ac2053eec5dd55e76c9f81f
unilock <unilock@fennet.rentals>
2023-09-05 15:56:55 -0400

    Fixes for 1.18.2
    Signed-off-by: unilock <unilock@fennet.rentals>

--- common/src/main/java/cc/unilock/glassbreaker/GlassBreaker.java
+++ common/src/main/java/cc/unilock/glassbreaker/GlassBreaker.java
@@ -1,13 +1,13 @@
 package cc.unilock.glassbreaker;
 
 import net.minecraft.item.Item;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.TagKey;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class GlassBreaker {
     public static final String MOD_ID = "glassbreaker";
-    public static final TagKey<Item> GLASS_ITEMS = TagKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, "glass_items"));
+    public static final TagKey<Item> GLASS_ITEMS = TagKey.of(Registry.ITEM_KEY, new Identifier(MOD_ID, "glass_items"));
 
     public static void init() {
     }
