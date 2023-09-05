9881b758987c303164d1bde66b42c83c2dcbdcc2
unilock <unilock@fennet.rentals>
2023-09-05 15:33:58 -0400

    Only check for pickaxes, not axes
    did you even read the mod description!?
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- common/src/main/java/cc/unilock/glassbreaker/mixin/MixinMiningToolItem.java
+++ common/src/main/java/cc/unilock/glassbreaker/mixin/MixinMiningToolItem.java
@@ -3,7 +3,10 @@ package cc.unilock.glassbreaker.mixin;
 import cc.unilock.glassbreaker.GlassBreaker;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.enums.Instrument;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.sound.BlockSoundGroup;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -28,14 +31,14 @@ public class MixinMiningToolItem extends Item {
 
     @Inject(method = "getMiningSpeedMultiplier", at = @At("HEAD"), cancellable = true)
     private void glassbreaker$getMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-        if (glassbreaker$isGlass(state) && (miningToolItem.equals(PickaxeItem.class) || miningToolItem.equals(AxeItem.class))) {
+        if (glassbreaker$isGlass(state) && glassbreaker$miningToolItem.equals(PickaxeItem.class)) {
             cir.setReturnValue(this.miningSpeed);
         }
     }
 
     @Inject(method = "isSuitableFor", at = @At("HEAD"), cancellable = true)
     private void glassbreaker$isSuitableFor(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-        if (glassbreaker$isGlass(state) && (miningToolItem.equals(PickaxeItem.class) || miningToolItem.equals(AxeItem.class))) {
+        if (glassbreaker$isGlass(state) && glassbreaker$miningToolItem.equals(PickaxeItem.class)) {
             cir.setReturnValue(true);
         }
     }
