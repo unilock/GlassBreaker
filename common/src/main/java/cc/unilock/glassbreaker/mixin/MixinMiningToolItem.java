c7bb4114fa9390106cf5af850e8a3c5e2cb5bac2
unilock <unilock@fennet.rentals>
2023-10-31 13:12:16 -0400

    Redirect BlockState#isIn, improving mod compatibility drastically
    Returns true when:
    - original call would return true
    - queried tag is BlockTags.PICKAXE_MINEABLE (better compat than checking if "this instanceof PickaxeItem"), and...
    - original "isGlass" (block item or material) is true
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- common/src/main/java/cc/unilock/glassbreaker/mixin/MixinMiningToolItem.java
+++ common/src/main/java/cc/unilock/glassbreaker/mixin/MixinMiningToolItem.java
@@ -1,52 +1,34 @@
 package cc.unilock.glassbreaker.mixin;
 
 import cc.unilock.glassbreaker.GlassBreaker;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.enums.Instrument;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
-import net.minecraft.item.PickaxeItem;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.sound.BlockSoundGroup;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Objects;
 
 @Mixin(MiningToolItem.class)
-public class MixinMiningToolItem extends Item {
-    public MixinMiningToolItem(Settings settings) {
-        super(settings);
+public class MixinMiningToolItem {
+    @Redirect(method = "getMiningSpeedMultiplier", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"))
+    private boolean getMiningSpeedMultiplier(BlockState instance, TagKey<Block> tagKey) {
+        return glassbreaker$tagCheck(instance, tagKey);
     }
 
-    @Shadow
-    @Final
-    protected float miningSpeed;
-
-    @Unique
-    private final Class<? extends Item> glassbreaker$miningToolItem = this.asItem().getClass();
-
-    @Inject(method = "getMiningSpeedMultiplier", at = @At("HEAD"), cancellable = true)
-    private void glassbreaker$getMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-        if (glassbreaker$isGlass(state) && glassbreaker$miningToolItem.equals(PickaxeItem.class)) {
-            cir.setReturnValue(this.miningSpeed);
-        }
-    }
-
-    @Inject(method = "isSuitableFor", at = @At("HEAD"), cancellable = true)
-    private void glassbreaker$isSuitableFor(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-        if (glassbreaker$isGlass(state) && glassbreaker$miningToolItem.equals(PickaxeItem.class)) {
-            cir.setReturnValue(true);
-        }
+    @Redirect(method = "isSuitableFor", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z", ordinal = 3))
+    private boolean isSuitableFor(BlockState instance, TagKey<Block> tagKey) {
+        return glassbreaker$tagCheck(instance, tagKey);
     }
 
     @Unique
-    private static boolean glassbreaker$isGlass(BlockState state) {
-        return state.getBlock().asItem().getDefaultStack().isIn(GlassBreaker.GLASS_ITEMS)
-                || state.getSoundGroup().equals(BlockSoundGroup.GLASS)
-                || state.getInstrument().equals(Instrument.HAT);
+    private static boolean glassbreaker$tagCheck(BlockState state, TagKey<Block> tag) {
+        return state.isIn(tag) || Objects.equals(tag, BlockTags.PICKAXE_MINEABLE) && (state.getBlock().asItem().getDefaultStack().isIn(GlassBreaker.GLASS_ITEMS) || Objects.equals(state.getSoundGroup(), BlockSoundGroup.GLASS) || Objects.equals(state.getInstrument(), Instrument.HAT));
     }
 }
