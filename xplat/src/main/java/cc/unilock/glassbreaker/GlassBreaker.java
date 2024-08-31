b48b1f05571c4f5a32a2ad2a93c1b1cf411e28ce
unilock <unilock@fennet.rentals>
2024-08-31 14:46:07 -0400

    redo for 1.20.3/.4

---
+++ xplat/src/main/java/cc/unilock/glassbreaker/GlassBreaker.java
@@ -0,0 +1,11 @@
+package cc.unilock.glassbreaker;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+public class GlassBreaker {
+    public static final String MOD_ID = "glassbreaker";
+    public static final TagKey<Item> GLASS_ITEMS = TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, "glass_items"));
+}
